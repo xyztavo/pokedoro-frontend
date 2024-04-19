540c627363d357165379f6ff1fa654e530db69c3
ustav <gustavobreasy@gmail.com>
2024-04-19 16:02:40 -0300

    fix: mobile elements taking too much space

--- src/components/user-pokemons.tsx
+++ src/components/user-pokemons.tsx
@@ -66,7 +66,7 @@ export default function UserPokemons() {
     const userData = getUser()
 
     return (
-        <div className="font-pixel text-[12px] flex flex-col items-center justify-center space-y-4 mt-4">
+        <div className="font-pixel text-[12px]">
             <div className="flex flex-col md:flex-row justify-center items-center gap-4">
                 <h1 className="font-semibold text-center text-lg my-4">{userData.name} pokemons:</h1>
                 <form onSubmit={(e) => {
